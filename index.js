ad4b2993387e462fd1fede5e13c0fad488d85c36
hosak94 <yigitcanhosak@gmail.com>
2023-05-17 21:53:40 +0300

    course work completed

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const server = require('./api/server');
-
+const server = require("./api/server");
+console.log("hello world!");
 const port = 9000;
 
+server.listen(port, () => {
+  console.log("server is listening on" + port);
+});
 // START YOUR SERVER HERE
